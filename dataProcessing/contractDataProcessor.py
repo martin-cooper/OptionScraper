478b6f0b512093052462d9579297b45ba423b843
martin-cooper <martin.cooper01@gmail.com>
2020-06-07 22:55:53 -0700

    Move contract data processing

---
+++ dataProcessing/contractDataProcessor.py
@@ -0,0 +1,34 @@
+import concurrent.futures
+import random
+from typing import List, Dict
+
+from api.yahooOptionsApi import YahooOptionsApi, DataGranularityPayload
+
+CONTRACT_NAME_SIZE = 200
+
+
+def chunkList(data: List, chunkSize: int) -> List[List]:
+    return [data[i: i + chunkSize] for i in range(0, len(data), chunkSize)]
+
+
+def aggregateAndNormalizeContractNames(contractNames: List[Dict[str, str]]) -> List[List[str]]:
+    aggregated = []
+    for contractList in contractNames:
+        calls = contractList['calls']
+        puts = contractList['puts']
+        aggregated.extend(calls + puts)
+    normalized = chunkList(aggregated, CONTRACT_NAME_SIZE)
+    return random.shuffle(normalized)
+
+
+def queryData(contractNames: List[str]):
+    yahoo = YahooOptionsApi()
+    return yahoo.execute(contractNames, DataGranularityPayload.INTRADAY)
+
+
+def getContractPricingData(contractNames: List[Dict[str, str]]):
+    THREADS = 30
+    contracts = aggregateAndNormalizeContractNames(contractNames)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=THREADS) as executor:
+        result = [res for res in executor.map(queryData, contracts)]
+        return result
--- main.py
+++ main.py
@@ -13,34 +13,6 @@ NASDAQ = "ATVI,ADBE,AMD,ALXN,ALGN,GOOG,GOOGL,AMZN,AMGN,ADI,ANSS,AAPL,AMAT,ASML,A
          "FISV,FOX,FOXA,GILD,IDXX,ILMN,INCY,INTC,INTU,ISRG,JD,KLAC,LRCX,LBTYA,LBTYK,LULU,MAR,MXIM,MELI,MCHP,MU,MSFT," \
          "MDLZ,MNST,NTAP,NTES,NFLX,NVDA,NXPI,ORLY,PCAR,PAYX,PYPL,PEP,QCOM,REGN,ROST,SGEN,SIRI,SWKS,SPLK,SBUX,SNPS," \
          "TMUS,TTWO,TSLA,TXN,KHC,TCOM,ULTA,UAL,VRSN,VRSK,VRTX,WBA,WDC,WDAY,XEL,XLNX,ZM"
-CONTRACT_NAME_SIZE = 200
-
-
-def chunkList(data: List, chunkSize: int) -> List[List]:
-    return [data[i: i + chunkSize] for i in range(0, len(data), chunkSize)]
-
-
-def aggregateAndNormalizeContractNames(contractNames: List[Dict[str, str]]) -> List[List[str]]:
-    aggregated = []
-    for contractList in contractNames:
-        calls = contractList['calls']
-        puts = contractList['puts']
-        aggregated.extend(calls + puts)
-    normalized = chunkList(aggregated, CONTRACT_NAME_SIZE)
-    return random.shuffle(normalized)
-
-
-def queryData(contractNames: List[str]):
-    yahoo = YahooOptionsApi()
-    return yahoo.execute(contractNames, DataGranularityPayload.INTRADAY)
-
-
-def getContractPricingData(contractNames: List[Dict[str, str]]):
-    THREADS = 30
-    contracts = aggregateAndNormalizeContractNames(contractNames)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=THREADS) as executor:
-        result = [res for res in executor.map(queryData, contracts)]
-        return result
 
 
 def main():
